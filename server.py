bb2da4057d6cf896a5079dd585d8a6dc48281485
DavidVelazquez584 <A01632648@itesm.mx>
2021-11-29 17:17:29 -0600

    Server fixed and new files added

--- server.py
+++ server.py
@@ -1,15 +1,13 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from TrafficM import runModel
 
 class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def __init__(self, dataJson):
+        self.data = dataJson
 
-        response = """{
-    \"data\": [
-        {\"x\":0, \"y\":1, \"z\":2},
-        {\"x\":3, \"y\":4, \"z\":5},
-        {\"x\":6, \"y\":7, \"z\":8}
-    ]
-}"""
+    def do_GET(self, dataJson):
+
+        response = dataJson
         # send 200 response
         self.send_response(200)
         # send response headers
@@ -17,5 +15,6 @@ class MyHandler(BaseHTTPRequestHandler):
         # send the body of the response
         self.wfile.write(bytes(response, "utf-8"))
 
-httpd = HTTPServer(('localhost', 8000), MyHandler)
+data = runModel()
+httpd = HTTPServer(('localhost', 8020), MyHandler.do_GET(data))
 httpd.serve_forever()
